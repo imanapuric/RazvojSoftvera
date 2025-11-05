02fa11c1b1848e94a27e3723424c4c188f1a545f
Imana Purić <imana.puric.23@size.ba>
2025-11-05 15:37:15 +0100

    Update pravougaonik.java
    Prvi commit

--- src/main/java/models/pravougaonik.java
+++ src/main/java/models/pravougaonik.java
@@ -2,10 +2,7 @@ package models;
 import interfaces.oblik;
 
 public class pravougaonik implements ispis<String, oblik>{
-    public pravougaonik(double visina, double sirina) {
-        this.visina = visina;
-        this.sirina = sirina;
-    }
+
 
 
 
